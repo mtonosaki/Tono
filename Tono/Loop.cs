54e2eaf071a197333f007f349747d9c5533324e2
Manabu Tonosaki <manabu@tomarika.com>
2020-02-28 13:52:49 +0900

    Add utilities : LoopUtil<T> Loop<T>

---
+++ Tono/Loop.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tono
+{
+    /// <summary>
+    /// Loop Utility
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// foreach (var item in LoopUtil<int>.From(data, out var cu))
+    /// {
+    ///     var index = cu.CurrentIndex;
+    ///     cu.DoFirstOneTime(() => { ... });   // Exec one time on the first circle
+    ///     cu.DoLastOneTime(() => { ... });    // Exec one time on the last circle
+    /// }
+    /// </remarks>
+    public class LoopUtil<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private IEnumerable<T> Collection;
+        private IEnumerator<T> Enumerator;
+        private bool IsFirstExec = false;
+        private Action LastAction = null;
+
+        public int CurrentIndex { get; private set; } = -1;
+        public T Current => Enumerator.Current;
+        object IEnumerator.Current => Enumerator.Current;
+
+        public static IEnumerable<T> From(IEnumerable<T> col, out LoopUtil<T> instance)
+        {
+            instance = new LoopUtil<T>
+            {
+                Collection = col,
+                Enumerator = col.GetEnumerator(),
+            };
+            return instance;
+        }
+
+        public void Dispose()
+        {
+            Enumerator.Dispose();
+            CurrentIndex = -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            var ret = Enumerator.MoveNext();
+            if (ret)
+            {
+                CurrentIndex++;
+            }
+            else
+            {
+                LastAction?.Invoke();
+            }
+            return ret;
+        }
+
+        public void Reset()
+        {
+            CurrentIndex = -1;
+            IsFirstExec = false;
+            LastAction = null;
+            Enumerator.Reset();
+        }
+
+        /// <summary>
+        /// Exec the first 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public void DoFirstOneTime(Action action)
+        {
+            if (IsFirstExec == false)
+            {
+                IsFirstExec = true;
+                action?.Invoke();
+            }
+        }
+
+        public void DoLastOneTime(Action action)
+        {
+            LastAction = action;
+        }
+    }
+
+
+    /// <summary>
+    /// Loop Utility
+    /// </summary>
+    public class Loop<TSource>
+    {
+        private enum ActionTypes
+        {
+            Break, Skip, Initialize, When, ForEach, Switch, SwitchDefault,
+        };
+        private IEnumerable<TSource> Data = null;
+        private List<(Func<TSource, bool> Condition, ActionTypes ActionType, Action<TSource, int> Action, int No)> Controls = new List<(Func<TSource, bool> Condition, ActionTypes ActionType, Action<TSource, int> Action, int No)>();
+        private List<Action<TSource, int>> Finalizes = new List<Action<TSource, int>>();
+
+        public int CurrentIndex { get; set; } = -1;
+        private bool IsInitialized { get; set; }
+
+        public static Loop<TSource> Build(IEnumerable<TSource> collection)
+        {
+            return new Loop<TSource>
+            {
+                Data = collection,
+            };
+        }
+
+        public Loop<TSource> Break(Func<TSource, bool> condition)
+        {
+            Controls.Add((condition, ActionTypes.Break, null, Controls.Count));
+            return this;
+        }
+        public Loop<TSource> Skip(Func<TSource, bool> condition)
+        {
+            Controls.Add((condition, ActionTypes.Skip, null, Controls.Count));
+            return this;
+        }
+        public Loop<TSource> Initialize(Action<TSource> action)
+        {
+            Controls.Add(((a => IsInitialized == false), ActionTypes.Initialize, (a, index) =>
+            {
+                action?.Invoke(a);
+                IsInitialized = true;
+            }, Controls.Count));
+            return this;
+        }
+        public Loop<TSource> Initialize(Action<TSource, int> action)
+        {
+            Controls.Add(((a => IsInitialized == false), ActionTypes.Initialize, (a, index) =>
+            {
+                action?.Invoke(a, index);
+                IsInitialized = true;
+            }, Controls.Count));
+            return this;
+        }
+        public Loop<TSource> When(Func<TSource, bool> condition, Action<TSource> action)
+        {
+            Controls.Add((condition, ActionTypes.When, ((a, index) => action?.Invoke(a)), Controls.Count));
+            return this;
+        }
+        public Loop<TSource> When(Func<TSource, bool> condition, Action<TSource, int> action)
+        {
+            Controls.Add((condition, ActionTypes.When, action, Controls.Count));
+            return this;
+        }
+        public Loop<TSource> ForEach(Action<TSource> action)
+        {
+            Controls.Add(((a => true), ActionTypes.ForEach, ((a, index) => action?.Invoke(a)), Controls.Count));
+            return this;
+        }
+        public Loop<TSource> ForEach(Action<TSource, int> action)
+        {
+            Controls.Add(((a => true), ActionTypes.ForEach, action, Controls.Count));
+            return this;
+        }
+
+        public Loop<TSource> Switch(Func<TSource, bool> condition, Action<TSource> action)
+        {
+            Controls.Add((condition, ActionTypes.Switch, ((a, index) => action?.Invoke(a)), Controls.Count));
+            return this;
+        }
+        public Loop<TSource> Switch(Func<TSource, bool> condition, Action<TSource, int> action)
+        {
+            Controls.Add((condition, ActionTypes.Switch, action, Controls.Count));
+            return this;
+        }
+
+        public Loop<TSource> SwitchDefault(Action<TSource> action)
+        {
+            Controls.Add(((a => true), ActionTypes.SwitchDefault, ((a, index) => action?.Invoke(a)), Controls.Count));
+            return this;
+        }
+        public Loop<TSource> SwitchDefault(Action<TSource, int> action)
+        {
+            Controls.Add(((a => true), ActionTypes.SwitchDefault, action, CurrentIndex));
+            return this;
+        }
+
+        public Loop<TSource> Finalize(Action<TSource> action)
+        {
+            Finalizes.Add((a, index) => action?.Invoke(a));
+            return this;
+        }
+        public Loop<TSource> Finalize(Action<TSource, int> action)
+        {
+            Finalizes.Add(action);
+            return this;
+        }
+
+        public Loop<TSource> Start()
+        {
+            if (Data == null) return this;
+
+            CurrentIndex = -1;
+            var isBreakeRequested = false;
+            IsInitialized = false;
+
+            Controls.Sort((a, b) =>
+            {
+                var r1 = (int)a.ActionType - (int)b.ActionType;
+                if (r1 == 0)
+                {
+                    return a.No - b.No;
+                }
+                else
+                {
+                    return r1;
+                }
+            });
+            TSource lastitem = default;
+            int lastIndex = -1;
+            foreach (var item in Data)
+            {
+                CurrentIndex++;
+                var isSwitched = false;
+                foreach (var cnt in Controls)
+                {
+                    if (cnt.Condition.Invoke(item))
+                    {
+                        if (cnt.ActionType == ActionTypes.Break)
+                        {
+                            isBreakeRequested = true;
+                            break;
+                        }
+                        else
+                        if (cnt.ActionType == ActionTypes.Skip)
+                        {
+                            break;
+                        }
+                        else
+                        if (cnt.ActionType == ActionTypes.Switch)
+                        {
+                            isSwitched = true;
+                        }
+                        else
+                        if (cnt.ActionType == ActionTypes.SwitchDefault && isSwitched)
+                        {
+                            break;  // Not execute cnt.Action
+                        }
+
+                        cnt.Action?.Invoke(item, CurrentIndex);
+                        lastitem = item;
+                        lastIndex = CurrentIndex;
+                    }
+                }
+                if (isBreakeRequested)
+                {
+                    break;
+                }
+            }
+            foreach (var cnt in Finalizes)
+            {
+                cnt.Invoke(lastitem, lastIndex);
+            }
+            return this;
+        }
+    }
+}
---
+++ UnitTestCore/LoopTest.cs
@@ -0,0 +1,327 @@
+// (c) 2019 Manabu Tonosaki
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Tono;
+
+namespace UnitTestCore
+{
+    [TestClass]
+    public class LoopTest
+    {
+        [TestMethod]
+        public void Test001()
+        {
+            var col = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            };
+
+            var st = new StringBuilder();
+            Loop<int>
+                .Build(col)
+                .ForEach(a =>
+                {
+                    st.Append($"{a}, ");
+                })
+                .When(a => a == 4, a =>
+                {
+                    st.Append($"W{a}, ");
+                })
+                .Skip(a =>
+                {
+                    return a % 2 == 1;
+                })
+                .Skip(a =>
+                {
+                    return a == 10;
+                })
+                .Break(a =>
+               {
+                   return a == 18;
+               })
+                .Initialize(a =>
+                {
+                    st.Append($"I{a}, ");
+                })
+                .Finalize(a =>
+               {
+                   st.Append($"F{a}, ");
+               })
+                .Start();
+            var ss = st.ToString();
+            Assert.AreEqual(ss, "I2, 2, W4, 4, 6, 8, 12, 14, 16, F16, ");
+        }
+
+        [TestMethod]
+        public void Test001R()
+        {
+            var col = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            };
+
+            var st = new StringBuilder();
+            Loop<int>
+                .Build(col)
+                .Skip(a =>
+                {
+                    return a % 2 == 1;
+                })
+                .Skip(a =>
+                {
+                    return a == 10;
+                })
+                .When(a => a == 4, a =>
+                {
+                    st.Append($"W{a}, ");
+                })
+                .Finalize(a =>
+                {
+                    st.Append($"F{a}, ");
+                })
+                .ForEach(a =>
+                {
+                    st.Append($"{a}, ");
+                })
+                .Break(a =>
+                {
+                    return a == 18;
+                })
+                .Initialize(a =>
+                {
+                    st.Append($"I{a}, ");
+                })
+                .Start();
+            var ss = st.ToString();
+            Assert.AreEqual("I2, 2, W4, 4, 6, 8, 12, 14, 16, F16, ", ss);
+        }
+
+        [TestMethod]
+        public void Test002()
+        {
+            var col = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            };
+
+            var st = new StringBuilder();
+            Loop<int>
+                .Build(col)
+                .Switch(a => a % 3 == 0, a =>
+                {
+                    st.Append($"A{a}, ");
+                })
+                .Switch(a => a % 3 == 1, a =>
+                {
+                    st.Append($"B{a}, ");
+                })
+                .SwitchDefault(a =>
+                {
+                    st.Append($"C{a}, ");
+                })
+                .Start();
+            var ss = st.ToString();
+            Assert.AreEqual("B1, C2, A3, B4, C5, A6, B7, C8, A9, B10, ", ss);
+        }
+
+        [TestMethod]
+        public void Test003()
+        {
+            var col = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            };
+
+            var st = new StringBuilder();
+            Loop<int>
+                .Build(col)
+                .Switch(a => a % 3 == 0, a =>
+                {
+                    st.Append($"A{a}, ");
+                })
+                .Switch(a => a % 3 == 1, a =>
+                {
+                    st.Append($"B{a}, ");
+                })
+                .SwitchDefault(a =>
+                {
+                    st.Append($"C{a}, ");
+                })
+                .Initialize(a =>
+               {
+                   st.Append($"I{a}, ");
+               })
+                .Finalize(a =>
+                {
+                    st.Append($"F{a}, ");
+                })
+                .ForEach(a =>
+                {
+                    st.Append($"{a}, ");
+                })
+                .Skip(a => a < 2)
+                .Start();
+            var ss = st.ToString();
+            Assert.AreEqual("I2, 2, C2, 3, A3, 4, B4, 5, C5, 6, A6, 7, B7, 8, C8, 9, A9, 10, B10, F10, ", ss);
+        }
+
+        [TestMethod]
+        public void Test004()
+        {
+            var col = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            };
+
+            var st = new StringBuilder();
+            Loop<int>
+                .Build(col)
+                .Skip(a => a < 2)
+                .Break(a => a > 9)
+                .ForEach((a, idx) =>
+                {
+                    Assert.AreEqual(a, col[idx]);
+                })
+                .ForEach((a, idx) =>
+                {
+                    Assert.AreEqual(a, idx + 1);
+                })
+                .Finalize((a, idx) =>
+                {
+                    Assert.AreEqual(8, idx);
+                })
+                .Initialize((a, idx) =>
+                {
+                    Assert.AreEqual(1, idx);
+                })
+                .Initialize((a, idx) =>
+                {
+                    Assert.AreEqual(2, a);
+                })
+                .Finalize((a, idx) =>
+                {
+                    Assert.AreEqual(9, a);
+                })
+                .Start();
+            var ss = st.ToString();
+        }
+
+        [TestMethod]
+        public void Test101()
+        {
+            var data = new List<int>
+            {
+                0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 
+            };
+            foreach (var item in LoopUtil<int>.From(data, out var cu))
+            {
+                Assert.AreEqual(item / 10, cu.CurrentIndex );
+            }
+        }
+
+        private IEnumerable<string> test102enum()
+        {
+            yield return "I-0";
+            yield return "I-1";
+            yield return "I-2";
+            yield return "I-3";
+            yield return "I-4";
+        }
+
+        [TestMethod]
+        public void Test102()
+        {
+            string lastitem = "agro;jhn";
+            foreach (var item in LoopUtil<string>.From(test102enum(), out var cu))
+            {
+                lastitem = item;
+                Assert.AreEqual($"I-{cu.CurrentIndex}", item);
+            }
+            Assert.AreEqual("I-4", lastitem);
+        }
+
+        [TestMethod]
+        public void Test103()
+        {
+            var dat = new Dictionary<int, string>
+            {
+                [10] = "S-10",
+                [20] = "S-20",
+                [30] = "S-30",
+                [40] = "S-40",
+                [50] = "S-50",
+                [60] = "S-60",
+                [70] = "S-70",
+                [80] = "S-80",
+                [90] = "S-90",
+            };
+            var c1 = 0;
+            var c2 = 0;
+            foreach (var item in LoopUtil<KeyValuePair<int,string>>.From(dat, out var cu))
+            {
+                cu.DoFirstOneTime(() =>
+                {
+                    Assert.AreEqual($"S-10", item.Value);
+                    c1++;
+                });
+                cu.DoLastOneTime(() =>
+                {
+                    Assert.AreEqual($"S-90", item.Value);
+                    c2++;
+                });
+                Assert.AreEqual($"S-{item.Key}", item.Value);
+            }
+            Assert.AreEqual(1, c1);
+            Assert.AreEqual(1, c2);
+        }
+        public void Test103m()
+        {
+            var dat = new Dictionary<int, string>
+            {
+                [10] = "S-10",
+                [20] = "S-20",
+                [30] = "S-30",
+                [40] = "S-40",
+                [50] = "S-50",
+                [60] = "S-60",
+                [70] = "S-70",
+                [80] = "S-80",
+                [90] = "S-90",
+            };
+            var c1 = 0;
+            var c2 = 0;
+            foreach (var item in LoopUtil<KeyValuePair<int, string>>.From(dat, out var cu))
+            {
+                cu.DoFirstOneTime(() =>
+                {
+                    Assert.AreEqual($"S-10", item.Value);
+                    c1++;
+                });
+                cu.DoFirstOneTime(() =>
+                {
+                    Assert.Fail("EXPECTING NOT EXEC Because of not 'first'");
+                    c1++;
+                });
+                cu.DoLastOneTime(() =>
+                {
+                    Assert.Fail("EXPECTING NOT EXEC Because of not 'last'");
+                    c2++;
+                });
+                cu.DoLastOneTime(() =>
+                {
+                    Assert.AreEqual($"S-90", item.Value);   // Override last action
+                    c2++;
+                });
+                Assert.AreEqual($"S-{item.Key}", item.Value);
+            }
+            Assert.AreEqual(1, c1); // Expecting One Time
+            Assert.AreEqual(1, c2); // Expecting One Time
+        }
+    }
+}
